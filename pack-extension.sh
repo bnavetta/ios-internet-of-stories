31162b114a6a510bf13aa2fb76206f983a7de49f
Ben Navetta <ben.navetta@gmail.com>
2017-06-25 11:21:11 -0700

    copy extension to dropbox automagically

--- pack-extension.sh
+++ pack-extension.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 CHROME="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 "$CHROME" --pack-extension="$PWD/chrome-extension" --pack-extension-key="$PWD/extension-key.pem"
+
+cp "$PWD/chrome-extension.crx" "$HOME/Dropbox/chrome-extension.crx"
